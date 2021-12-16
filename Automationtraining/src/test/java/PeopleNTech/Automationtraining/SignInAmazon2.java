ec2695132f5ac7338012710b69625a41d9e6e7c2
IGU Staff <IGU Staff@LAPTOP-J64VV9VG>
2021-12-15 23:03:11 -0500

    testng

--- Automationtraining/src/test/java/PeopleNTech/Automationtraining/SignInAmazon2.java
+++ Automationtraining/src/test/java/PeopleNTech/Automationtraining/SignInAmazon2.java
@@ -3,7 +3,6 @@ package PeopleNTech.Automationtraining;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-import Common.Utility;
 import PageObjectModel.Home;
 import PageObjectModel.SignInPage;
 
@@ -22,8 +21,8 @@ import org.openqa.selenium.io.FileHandler;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 
-public class SignInAmazon extends Utility {
-  
+public class SignInAmazon2 {
+  public WebDriver driver;
 	@Test(priority = 2,dependsOnMethods ="VerificationOfText" )
     public void f() throws InterruptedException {
 		Home ob=new Home(driver);
@@ -56,5 +55,45 @@ public class SignInAmazon extends Utility {
 	System.out.println("This is after assertion");
 	soft.assertAll();
 	}
-  
+  @BeforeMethod
+  public void beforeMethod() {
+     browsers("Edge","https://www.amazon.com"); 
+      
+      
+  }
+
+  @AfterMethod
+  public void afterMethod() throws InterruptedException {
+	  Thread.sleep(6000);
+	  driver.quit();
+  }
+  public void browsers(String browser,String wbsite) {
+	 String local= System.getProperty("user.dir");
+	 System.out.println(local);
+	  if(browser.equalsIgnoreCase("Chrome")) {
+		  System.setProperty("webdriver.chrome.driver", local+"\\src\\Drivers\\chromedriver.exe");
+		     driver=new ChromeDriver();
+		     driver.navigate().to(wbsite);
+		      driver.manage().window().maximize();
+	  }else if(browser.equalsIgnoreCase("Edge")) {
+		  System.setProperty("webdriver.edge.driver",local+"\\src\\Drivers\\msedgedriver.exe" );
+	      driver=new EdgeDriver();
+	      driver.navigate().to(wbsite);
+	      driver.manage().window().maximize();
+		  
+	  }else {
+		  System.out.println("Correct browser name");
+	  }
+	  
+  }
+  public void shots() throws IOException {
+	  Date dt=new Date();
+	  System.out.println(dt);
+	  String it=dt.toString().replace(" ", "_").replace(":","_");
+	  System.out.println(it);
+	  String local= System.getProperty("user.dir");
+	 File stored= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+	 FileHandler.copy(stored, new File(local+"\\Pictures\\"+it+"captured.jpg"));
+  }
+
 }
